022fc73cfb66aa77ba27c66709aaa29f6840243f
Crispin R <crispinr003@gmail.com>
2021-04-11 00:23:07 +0530

    Made some small changes

--- Mr.Bot.py
+++ Mr.Bot.py
@@ -5,30 +5,30 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 
-@app.route('/bot', methods=['POST'])
+@app.route("/bot", methods=["POST"])
 def bot():
-    incoming_msg = request.values.get('Body', '').lower()
+    incoming_msg = request.values.get("Body", "").lower()
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
-    if 'quote' in incoming_msg:
-        # return a quote
-        r = requests.get('https://api.quotable.io/random')
+    if "quote" in incoming_msg:
+        # Now to return a quote
+        r = requests.get("https://api.quotable.io/random")
         if r.status_code == 200:
             data = r.json()
             quote = f'{data["content"]} ({data["author"]})'
         else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
+            quote = "I could not retrieve a quote at this time, sorry!"
         msg.body(quote)
         responded = True
-    if 'cat' in incoming_msg:
+    if "cat" in incoming_msg:
         # return a cat pics
-        msg.media('https://cataas.com/cat')
+        msg.media("https://cataas.com/cat")
         responded = True
     if not responded:
-        msg.body('I only know about famous quotes and cats, sorry!')
+        msg.body("I only know about famous quotes and cats, sorry!")
     return str(resp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
