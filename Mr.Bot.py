7b5fa58d294e86d50166538dad8e9ff6a5191af6
Crispin R <crispinr003@gmail.com>
2021-05-18 22:40:25 +0530

    Made minor changes

--- Mr.Bot.py
+++ Mr.Bot.py
@@ -120,6 +120,9 @@ def bot():
         msg.body(reply)
     return str(resp)
 
+@app.route("/")
+def hello():
+    return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="localhost", port=5000, debug=True)
